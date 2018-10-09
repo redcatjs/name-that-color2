8ed90d3337437fb8769d2fa21ea401f1b5af7994
takion <jo@surikat.pro>
2018-10-09 02:29:31 +0200

    replace map approach (was too heavy)

--- cli.js
+++ cli.js
@@ -1,8 +1,56 @@
 #! /usr/bin/env node
 const oneColor = require('onecolor')
 const chalk = require('chalk')
-const fs = require('fs')
+const ntc = require('./ntc')
 
-const map = JSON.parse(fs.readFileSync('./colorsMap.json'))
+let color = oneColor(process.argv[2]).hex()
+let [ hexStart, name, exact ] = ntc.name(color)
 
-console.log(chalk.magenta(process.argv[2]) + ' name is ' + chalk.cyan(map[oneColor(process.argv[2]).hex()][1]))
+color = color.slice(1)
+hexStart = hexStart.slice(1)
+
+function colName(n) {
+  var ordA = 'a'.charCodeAt(0)
+  var ordZ = 'z'.charCodeAt(0)
+  var len = ordZ - ordA + 1
+
+  var s = "";
+  while(n >= 0) {
+    s = String.fromCharCode(n % len + ordA) + s
+    n = Math.floor(n / len) - 1
+  }
+  return s
+}
+
+function toSnakeCase(name){
+  name = name.replace(/\'/g, '')
+  name = name.replace(/^[^A-Za-z0-9]*|[^A-Za-z0-9]*$/g, '')
+    .replace(/([a-z])([A-Z])/g, (m, a, b) => a + '-' + b.toLowerCase())
+    .replace(/[^A-Za-z0-9]+|_+/g, '-')
+    .toLowerCase()
+  return name
+}
+
+name = toSnakeCase(name)
+
+if(!exact){
+  let cursorUp = parseInt(hexStart,16)
+  let cursorDown = parseInt(hexStart,16)
+  let cursor = 0
+  while(true){
+    cursor++
+    if(cursor%2 == 0){
+      cursorUp++
+      if(cursorUp.toString(16).padStart(6, '0')===color)
+        break
+    }
+    else{
+      cursorDown--
+      if(cursorDown.toString(16).padStart(6, '0')===color)
+        break
+    }
+  }
+  name += '-'+(colName(cursor).padStart(4, 'a'))
+}
+
+console.log(chalk.magenta(process.argv[2]) + ' name is ' + chalk.cyan(name))
